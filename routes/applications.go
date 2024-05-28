6d7bb8ecaee249a4c9e067093348549152bf6bde
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-05-28 23:51:51 +0300

    add application route

---
+++ routes/applications.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/weldonkipchirchir/job-listing-server/controllers"
+	"github.com/weldonkipchirchir/job-listing-server/db"
+	"github.com/weldonkipchirchir/job-listing-server/handler"
+	"github.com/weldonkipchirchir/job-listing-server/middleware"
+)
+
+func ApplicationRoutes(router *gin.Engine) {
+	errorHandler := handler.NewErrorHandler()
+	applicationHandler := controllers.NewApplicationHandler(db.GetCollection("applications"), errorHandler)
+	applicationGroup := router.Group("/api/v1/applications")
+	applicationGroup.Use(middleware.Authentication())
+	{
+		applicationGroup.GET("/admin", applicationHandler.GetAdminApplications)
+		applicationGroup.GET("/", applicationHandler.GetApplications)
+		applicationGroup.POST("/", applicationHandler.CreateApplications)
+		applicationGroup.PUT("/admin/:id", applicationHandler.EditApplication)
+		applicationGroup.DELETE("/:id", applicationHandler.DeleteApplication)
+	}
+}
