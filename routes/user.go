2d366b3649260dc1abde42eeb19367267ff18128
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-05-31 23:49:59 +0300

    add setting route

--- routes/user.go
+++ routes/user.go
@@ -3,16 +3,24 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/weldonkipchirchir/job-listing-server/controllers"
+	"github.com/weldonkipchirchir/job-listing-server/middleware"
 )
 
-func SetUpusers(router *gin.Engine) {
+func SetUpUsers(router *gin.Engine) {
 	users := router.Group("/api/v1/users/")
 	{
 		// login users
 		users.POST("/login", controllers.Login)
 		// Create a new user
 		users.POST("/register", controllers.Register)
-		//logout users
+		// logout users
 		users.POST("/logout", controllers.Logout)
+
+		// Apply middleware to all subsequent routes within the users group
+		users.Use(middleware.Authentication())
+		{
+			// update user settings
+			users.PUT("/settings", controllers.Settings)
+		}
 	}
 }
