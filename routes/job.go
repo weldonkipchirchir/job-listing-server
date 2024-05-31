22605887ba0e2ef292ded40b2c7e1c9598aa4b8a
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-05-31 23:48:49 +0300

    add getalljobs & GetjobById routes

--- routes/job.go
+++ routes/job.go
@@ -12,10 +12,14 @@ func JobRoutes(router *gin.Engine) {
 	errorHandler := handler.NewErrorHandler()
 	jobHandler := controllers.NewJobCollection(db.GetCollection("jobs"), errorHandler)
 	jobGroup := router.Group("/api/v1/jobs")
+	jobGroup.GET("/sponsored", jobHandler.GetSponsoredJobs)
 	jobGroup.Use(middleware.Authentication())
 	{
 		jobGroup.GET("/", jobHandler.GetAllJobs)
+		jobGroup.GET("/:id", jobHandler.GetJobById)
+		jobGroup.GET("/admin", jobHandler.GetAdminsJobs)
 		jobGroup.POST("/create", jobHandler.CreateJob)
-		jobGroup.PUT("/update/:id", jobHandler.Updatejob)
+		jobGroup.PUT("/admin/:id", jobHandler.Updatejob)
+		jobGroup.DELETE("/admin/:id", jobHandler.DeleteJob)
 	}
 }
