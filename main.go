8626eb6fb5826536455573756ba66f658be0bb5f
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-05-31 23:53:38 +0300

    add cors middleware

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/weldonkipchirchir/job-listing-server/db"
+	"github.com/weldonkipchirchir/job-listing-server/middleware"
 	"github.com/weldonkipchirchir/job-listing-server/routes"
 )
 
@@ -22,14 +24,30 @@ func main() {
 		log.Fatalf("Error connecting to the database: %v", err)
 	}
 
+	limiter := middleware.NewRateLimiter(10, 20)
+	router.Use(limiter.Middleware())
+
+	router.Use(cors.New(
+		cors.Config{
+			AllowOrigins:     []string{"http://localhost:5173"},
+			AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+			AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+			ExposeHeaders:    []string{"Content-Length"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+		}))
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	routes.SetUpusers(router)
+	routes.SetUpUsers(router)
 	routes.JobRoutes(router)
+	routes.ApplicationRoutes(router)
+	routes.SearchLog(router)
+	routes.BookmarksRoutes(router)
 
 	//create server
 	serv := &http.Server{
